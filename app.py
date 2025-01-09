00e6f8fae89751c636ef757d41ea6e2a3b098e86
tradaokamsa <hainamdao05@gmail.com>
2025-01-09 23:50:18 +0700

    create API

--- app.py
+++ app.py
@@ -1,42 +1,37 @@
-# import os
-# os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-#
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-#
-# from src.base.llm_model import get_hf_llm
-# from src.rag.main import build_rag_chain, InputQA, OutputQA
+import os
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.rag.main import build_rag_chain, InputQA, OutputQA
 from src.base.llm_model import get_hf_llm
-from src.rag.main import build_rag_chain
 
 llm = get_hf_llm(model_name = "openai-community/gpt2", temperature=0.9)
 genai_docs = "./data/files"
 
 genai_chain = build_rag_chain(llm, data_dir=genai_docs, data_type="pdf")
 
-# app = FastAPI(
-#     title="LangChain Server",
-#     description="A simple API server for Q&A using LangChain",
-#     version="1.0"
-# )
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-#     expose_headers=["*"]
-# )
+app = FastAPI(
+    title="LangChain Server",
+    description="A simple API server for Q&A using LangChain",
+    version="1.0"
+)
 
-# @app.get("/check")
-# async def check():
-#     return {"status": "ok"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
 
-# @app.post("/generative_ai", response_model=OutputQA)
-# async def generative_ai(input_qa: InputQA):
-#     answer = genai_chain.invoke(input_qa.question)
-#     return {"answer": answer}
+@app.get("/check")
+async def check():
+    return {"status": "ok"}
 
-answer = genai_chain.invoke("What is Attention?")
-print(answer)
+@app.post("/generative_ai", response_model=OutputQA)
+async def generative_ai(input_qa: InputQA):
+    answer = genai_chain.invoke(input_qa.question)
+    return {"answer": answer}
